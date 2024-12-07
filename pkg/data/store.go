d2087962966b95185113d53bdc4fe1e50f72eda0
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-12-07 08:11:58 +0000

    Adding docker compose

--- pkg/data/store.go
+++ pkg/data/store.go
@@ -1,5 +1,12 @@
 package data
 
+import "time"
+
+type SortedSetMember struct {
+	Score  float64
+	Member string
+}
+
 type Store interface {
 	Connect()
 	Disconnect()
@@ -29,4 +36,14 @@ type Store interface {
 	Unnotify(subscriptionId string)
 	UnnotifyCallback(subscriptionId string, callback NotificationCallback)
 	ProcessNotifications()
+
+	TempSet(key string, value string, expiration time.Duration) bool
+	TempGet(key string) string
+	TempExpire(key string, expiration time.Duration)
+	TempDel(key string)
+
+	SortedSetAdd(key string, member string, score float64) int64
+	SortedSetRemove(key string, member string) int64
+	SortedSetRemoveRangeByRank(key string, start, stop int64) int64
+	SortedSetRangeByScoreWithScores(key string, min, max string) []SortedSetMember
 }
