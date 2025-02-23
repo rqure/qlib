3fba0789de8a558d402494e43ea0ab05136c9bfb
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2025-02-23 22:47:49 +0000

    some progress

--- pkg/data/store/store.go
+++ pkg/data/store/store.go
@@ -4,6 +4,7 @@ import (
 	"github.com/rqure/qlib/pkg/data"
 	"github.com/rqure/qlib/pkg/data/store/postgres"
 	"github.com/rqure/qlib/pkg/data/store/redis"
+	"github.com/rqure/qlib/pkg/data/store/web"
 	"github.com/rqure/qlib/pkg/data/transformer"
 )
 
@@ -67,6 +68,27 @@ func NotifyOverRedis(address string, password string) ConfigFn {
 
 		store.ModifiableNotificationConsumer = redis.NewNotificationConsumer(core)
 		store.ModifiableNotificationPublisher = redis.NewNotificationPublisher(core)
+	}
+}
+
+func PersistOverWeb(address string) ConfigFn {
+	return func(store *Store) {
+		core := web.NewCore(web.Config{Address: address})
+
+		store.MultiConnector.AddConnector(web.NewConnector(core))
+		store.ModifiableSchemaManager = web.NewSchemaManager(core)
+		store.ModifiableEntityManager = web.NewEntityManager(core)
+		store.ModifiableFieldOperator = web.NewFieldOperator(core)
+		store.ModifiableSnapshotManager = web.NewSnapshotManager(core)
+	}
+}
+
+func NotifyOverWeb(address string) ConfigFn {
+	return func(store *Store) {
+		core := web.NewCore(web.Config{Address: address})
+
+		store.ModifiableNotificationConsumer = web.NewNotificationConsumer(core)
+		store.ModifiableNotificationPublisher = web.NewNotificationPublisher(core)
 	}
 }
 
