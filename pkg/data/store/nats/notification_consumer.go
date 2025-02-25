9de118fb03aacd9d8a5c900d4a75bc7ace3ff991
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2025-02-25 17:58:56 +0000

    progress

--- pkg/data/store/nats/notification_consumer.go
+++ pkg/data/store/nats/notification_consumer.go
@@ -38,11 +38,11 @@ func NewNotificationConsumer(core Core) data.ModifiableNotificationConsumer {
 	return consumer
 }
 
-func (n *NotificationConsumer) SetTransformer(t data.Transformer) {
-	n.transformer = t
+func (me *NotificationConsumer) SetTransformer(t data.Transformer) {
+	me.transformer = t
 }
 
-func (n *NotificationConsumer) handleNotification(msg *nats.Msg) {
+func (me *NotificationConsumer) handleNotification(msg *nats.Msg) {
 	apiMsg := &protobufs.ApiMessage{}
 	if err := proto.Unmarshal(msg.Data, apiMsg); err != nil {
 		log.Error("Failed to unmarshal web message: %v", err)
@@ -56,24 +56,24 @@ func (n *NotificationConsumer) handleNotification(msg *nats.Msg) {
 	}
 
 	notif := notification.FromPb(&notifPb)
-	n.pendingNotifs <- notif
+	me.pendingNotifs <- notif
 }
 
-func (n *NotificationConsumer) ProcessNotifications(ctx context.Context) {
+func (me *NotificationConsumer) ProcessNotifications(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
 			return
-		case notif := <-n.pendingNotifs:
-			if callbacks, ok := n.callbacks[notif.GetToken()]; ok {
+		case notif := <-me.pendingNotifs:
+			if callbacks, ok := me.callbacks[notif.GetToken()]; ok {
 				for _, cb := range callbacks {
 					cb.Fn(context.Background(), notif)
 				}
 			}
-		case <-n.keepAlive.C:
+		case <-me.keepAlive.C:
 			// Resend all registered notifications to keep them alive
-			for token := range n.callbacks {
-				n.sendNotify(ctx, notification.FromToken(token))
+			for token := range me.callbacks {
+				me.sendNotify(ctx, notification.FromToken(token))
 			}
 		default:
 			return
@@ -81,7 +81,7 @@ func (n *NotificationConsumer) ProcessNotifications(ctx context.Context) {
 	}
 }
 
-func (n *NotificationConsumer) sendNotify(ctx context.Context, config data.NotificationConfig) (string, error) {
+func (me *NotificationConsumer) sendNotify(ctx context.Context, config data.NotificationConfig) (string, error) {
 	if config.GetServiceId() == "" {
 		config.SetServiceId(app.GetName())
 	}
@@ -90,7 +90,7 @@ func (n *NotificationConsumer) sendNotify(ctx context.Context, config data.Notif
 		Requests: []*protobufs.DatabaseNotificationConfig{notification.ToConfigPb(config)},
 	}
 
-	resp, err := n.core.Request(ctx, n.core.GetKeyGenerator().GetNotificationSubject(), msg)
+	resp, err := me.core.Request(ctx, me.core.GetKeyGenerator().GetNotificationSubject(), msg)
 	if err != nil {
 		return "", err
 	}
@@ -108,46 +108,46 @@ func (n *NotificationConsumer) sendNotify(ctx context.Context, config data.Notif
 	return token, nil
 }
 
-func (n *NotificationConsumer) Notify(ctx context.Context, config data.NotificationConfig, cb data.NotificationCallback) data.NotificationToken {
-	tokenId, err := n.sendNotify(ctx, config)
+func (me *NotificationConsumer) Notify(ctx context.Context, config data.NotificationConfig, cb data.NotificationCallback) data.NotificationToken {
+	tokenId, err := me.sendNotify(ctx, config)
 
 	if err == nil {
-		n.callbacks[tokenId] = append(n.callbacks[tokenId], cb)
-		return notification.NewToken(tokenId, n, cb)
+		me.callbacks[tokenId] = append(me.callbacks[tokenId], cb)
+		return notification.NewToken(tokenId, me, cb)
 	} else {
 		log.Error("notification registration failed: %v", err)
 	}
 
-	return notification.NewToken("", n, nil)
+	return notification.NewToken("", me, nil)
 }
 
-func (n *NotificationConsumer) Unnotify(ctx context.Context, token string) {
+func (me *NotificationConsumer) Unnotify(ctx context.Context, token string) {
 	msg := &protobufs.ApiRuntimeUnregisterNotificationRequest{
 		Tokens: []string{token},
 	}
 
-	_, err := n.core.Request(ctx, n.core.GetKeyGenerator().GetNotificationSubject(), msg)
+	_, err := me.core.Request(ctx, me.core.GetKeyGenerator().GetNotificationSubject(), msg)
 	if err != nil {
 		log.Error("Failed to unregister notification: %v", err)
 	}
-	delete(n.callbacks, token)
+	delete(me.callbacks, token)
 }
 
-func (n *NotificationConsumer) UnnotifyCallback(ctx context.Context, token string, cb data.NotificationCallback) {
-	if n.callbacks[token] == nil {
+func (me *NotificationConsumer) UnnotifyCallback(ctx context.Context, token string, cb data.NotificationCallback) {
+	if me.callbacks[token] == nil {
 		return
 	}
 
 	callbacks := []data.NotificationCallback{}
-	for _, callback := range n.callbacks[token] {
+	for _, callback := range me.callbacks[token] {
 		if callback.Id() != cb.Id() {
 			callbacks = append(callbacks, callback)
 		}
 	}
 
 	if len(callbacks) == 0 {
-		n.Unnotify(ctx, token)
+		me.Unnotify(ctx, token)
 	} else {
-		n.callbacks[token] = callbacks
+		me.callbacks[token] = callbacks
 	}
 }
