d503c6b18173d07eceaf7aa7275b8ed846eaf04e
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2025-03-10 16:37:16 +0000

    some fixes

--- pkg/data/field/entity_list.go
+++ pkg/data/field/entity_list.go
@@ -8,24 +8,28 @@ import (
 
 // EntityListImpl implements the EntityList interface
 type EntityListImpl struct {
-	entities []string
+	entities *[]string
 }
 
 // NewEntityList creates a new EntityList with the given entities
-func NewEntityList(entities []string) data.EntityList {
+func NewEntityList(entities *[]string) data.EntityList {
+	if entities == nil {
+		entities = &[]string{}
+	}
+
 	return &EntityListImpl{
 		entities: entities,
 	}
 }
 
 func (e *EntityListImpl) GetEntities() []string {
-	result := make([]string, len(e.entities))
-	copy(result, e.entities)
+	result := make([]string, e.Length())
+	copy(result, e.getEntities())
 	return result
 }
 
 func (e *EntityListImpl) SetEntities(entities []string) data.EntityList {
-	e.entities = entities
+	e.setEntities(entities)
 	return e
 }
 
@@ -33,31 +37,62 @@ func (e *EntityListImpl) Add(entity string) bool {
 	if e.Contains(entity) {
 		return false
 	}
-	e.entities = append(e.entities, entity)
+	entities := e.getEntities()
+	entities = append(entities, entity)
+	e.setEntities(entities)
 	return true
 }
 
 func (e *EntityListImpl) Remove(entity string) bool {
-	for i, id := range e.entities {
-		if id == entity {
-			// Remove by swapping with last element and truncating
-			e.entities[i] = e.entities[len(e.entities)-1]
-			e.entities = e.entities[:len(e.entities)-1]
-			return true
-		}
+	if !e.Contains(entity) {
+		return false
 	}
-	return false
+	e.entities = slices.Remove(*e.entities, entity)
+	return true
 }
 
 func (e *EntityListImpl) Contains(entity string) bool {
-	return slices.Contains(e.entities, entity)
+	return slices.Contains(*e.entities, entity)
 }
 
 func (e *EntityListImpl) Count() int {
-	return len(e.entities)
+	return len(e.getEntities())
 }
 
 func (e *EntityListImpl) Clear() data.EntityList {
-	e.entities = []string{}
+	e.setEntities([]string{})
 	return e
+}
+
+func (e *EntityListImpl) At(index int) string {
+	es := e.getEntities()
+	return es[index]
+}
+
+func (e *EntityListImpl) SetAt(index int, entity string) data.EntityList {
+	es := e.getEntities()
+	es[index] = entity
+	return e
+}
+
+func (e *EntityListImpl) Last() string {
+	es := e.getEntities()
+	return es[e.Length()-1]
+}
+
+func (e *EntityListImpl) First() string {
+	es := e.getEntities()
+	return es[0]
+}
+
+func (e *EntityListImpl) Length() int {
+	return len(e.getEntities())
+}
+
+func (e *EntityListImpl) getEntities() []string {
+	return *e.entities
+}
+
+func (e *EntityListImpl) setEntities(entities []string) {
+	e.entities = &entities
 }
