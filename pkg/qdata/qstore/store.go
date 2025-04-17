42a1410230ba3dcd194ce5e1847c2c600d0e5496
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2025-04-17 14:44:46 +0000

    Fixin bugs

--- pkg/qdata/qstore/store.go
+++ pkg/qdata/qstore/store.go
@@ -1,14 +1,26 @@
 package qstore
 
-import "github.com/rqure/qlib/pkg/qdata"
+import (
+	"os"
+
+	"github.com/rqure/qlib/pkg/qdata"
+)
+
+func getFromEnvOrDefault(key string, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
 
 func New(opts ...qdata.StoreOpts) *qdata.Store {
 	store := new(qdata.Store).ApplyOpts(opts...)
 
 	if len(opts) == 0 {
 		opts = []qdata.StoreOpts{
-			NotifyOverNats("nats://nats:4222"),
-			PersistOverRedis("redis://redis:6379", "", 0, 10),
+			NotifyOverNats(getFromEnvOrDefault("Q_NATS_URL", "nats://nats:4222")),
+			PersistOverRedis(getFromEnvOrDefault("Q_REDIS_URL", "redis://redis:6379"), "", 0, 10),
 		}
 	}
 
