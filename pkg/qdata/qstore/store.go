b84699b8982d3e7f8d5d91f3b711de418bb76f89
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2025-05-07 19:52:23 +0000

    Adding ws support

--- pkg/qdata/qstore/store.go
+++ pkg/qdata/qstore/store.go
@@ -4,7 +4,6 @@ import (
 	"os"
 
 	"github.com/rqure/qlib/pkg/qdata"
-	"github.com/rqure/qlib/pkg/qdata/qstore/qnats"
 )
 
 func getFromEnvOrDefault(key string, defaultValue string) string {
@@ -15,18 +14,14 @@ func getFromEnvOrDefault(key string, defaultValue string) string {
 	return value
 }
 
-func DefaultNatsAddress() string {
-	return getFromEnvOrDefault("Q_NATS_URL", "nats://nats:4222")
-}
-
-func DefaultRedisAddress() string {
-	return getFromEnvOrDefault("Q_REDIS_URL", "redis:6379")
+func DefaultWebsocketAddress() string {
+	return getFromEnvOrDefault("Q_CORE_WS_URL", "ws://localhost:7860")
 }
 
 func New(opts ...qdata.StoreOpts) *qdata.Store {
 	if len(opts) == 0 {
 		opts = []qdata.StoreOpts{
-			CommunicateOverNats(DefaultNatsAddress()),
+			CommunicateOverWebSocket(DefaultWebsocketAddress()),
 		}
 	}
 
@@ -35,10 +30,10 @@ func New(opts ...qdata.StoreOpts) *qdata.Store {
 	return store
 }
 
-func New2(natsCore qnats.NatsCore) *qdata.Store {
+func New2() *qdata.Store {
 	opts := []qdata.StoreOpts{
 		PersistInMemoryBadger(),
-		NotifyOverNatsWithCore(natsCore),
+		NotifyOverWebSocket(DefaultWebsocketAddress()),
 	}
 
 	store := new(qdata.Store).Init(opts...)
